f31c8af7f49cd02584d7825b68c5fadebc5afc62
Levi <christmas.19889@gmail.com>
2025-04-20 17:18:30 +0300

    init and manager app

---
+++ manager-app/src/main/java/com/leviathan/manager/repo/InMemoryProductRepo.java
@@ -0,0 +1,46 @@
+package com.leviathan.manager.repo;
+
+import com.leviathan.manager.entity.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+@Repository
+public class InMemoryProductRepo implements ProductRepository {
+
+    private final List<Product> products = Collections.synchronizedList(new LinkedList<>());
+
+    public InMemoryProductRepo() {
+        IntStream.range(1, 4)
+                .forEach(i -> this.products.add(new Product(i, "Product №%d".formatted(i),
+                        "Product details №%d".formatted(i))));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return Collections.unmodifiableList(products);
+    }
+
+    @Override
+    public Product save(Product product) {
+        product.setId(this.products.stream()
+                .max(Comparator.comparingInt(Product::getId))
+                .map(Product::getId)
+                .orElse(0) + 1);
+        this.products.add(product);
+        return product;
+    }
+
+    @Override
+    public Optional<Product> findById(Integer productId) {
+        return this.products.stream()
+                .filter(product -> Objects.equals(product.getId(), productId))
+                .findFirst();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        this.products.removeIf(product -> Objects.equals(product.getId(), id));
+    }
+}
---
+++ manager-app/src/main/java/com/leviathan/manager/repo/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.leviathan.manager.repo;
+
+import com.leviathan.manager.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> findAll();
+
+    Product save(Product product);
+
+    Optional<Product> findById(Integer productId);
+
+    void deleteById(Integer id);
+}
