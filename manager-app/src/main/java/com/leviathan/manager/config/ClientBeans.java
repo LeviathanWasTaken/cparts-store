d835343807b9fe87f57300de1341e7d95a2244f5
Levi <christmas.19889@gmail.com>
2025-05-05 12:10:45 +0300

    Keycloak integration

--- manager-app/src/main/java/com/leviathan/manager/config/ClientBeans.java
+++ manager-app/src/main/java/com/leviathan/manager/config/ClientBeans.java
@@ -1,9 +1,13 @@
 package com.leviathan.manager.config;
 
 import com.leviathan.manager.client.RestClientProductsRestClient;
+import com.leviathan.manager.security.OAuthClientHttpRequestInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -11,10 +15,17 @@ public class ClientBeans {
 
     @Bean
     public RestClientProductsRestClient restClientProductsRestClient(
-            @Value("${cparts-store.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri
+            @Value("${cparts-store.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository,
+            @Value("${cparts-store.services.catalogue.registration-id:keycloak}") String registrationId
     ) {
         return new RestClientProductsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
+                .requestInterceptor(
+                        new OAuthClientHttpRequestInterceptor(
+                            new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                                    authorizedClientRepository), registrationId))
                 .build());
     }
 }
